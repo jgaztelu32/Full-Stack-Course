aa2edc86e27365c44b23b10c15563366e9cdbf3a
Jesus Gaztelu <jesusito@jesusgaztelu.es>
2025-12-27 13:22:43 +0100

    MongoDB connection

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,11 @@
-console.log("Hello World!")
+const express = require('express');
+const dotenv = require('dotenv').config();
+const port = process.env.PORT || 8000;
+
+const app = express();
+
+app.get('/api/users', (req,resp) => {
+    resp.json({ message: 'get user'});
+})
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
